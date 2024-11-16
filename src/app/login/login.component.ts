5c245e35859184372b324fef86912c32a9db6bd9
sree <sreelakshmiajayan2k@gmail.com>
2024-11-16 20:09:25 +0530

    register and login added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [CommonModule,HttpClientModule,ReactiveFormsModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loading = false;
+  error = '';
 
-isLoginClicked:boolean=false;
+  constructor(private formBuilder: FormBuilder,private authService: AuthService,private router: Router)
+  {
+    this.loginForm = this.formBuilder.group({
+      
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
 
-  loginGroup = new FormGroup(
-    {
-      userName:new FormControl('',[Validators.required]),
-      password:new FormControl('',[Validators.required])
-    }
-  )
-  loginUser() {
-    this.isLoginClicked=true;
-    if(!this.loginGroup.dirty)
-    {
-   
-    }
- }
+    this.loading = true;
+    this.authService.login(
+      this.loginForm.get('username')?.value,
+      this.loginForm.get('password')?.value
+    ).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: error => {
+        if (error.status === 403) {
+          this.error = 'Invalid username or password';
+        } else {
+          this.error = 'Unable to connect to the server';
+        }
+        this.loading = false;
+      }
+    });
+    
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { AuthResponse } from '../model/auth.model';
+import { DOCUMENT } from '@angular/common';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class AuthService {
+    private apiUrl = 'http://localhost:8080/api/auth';
+    private httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    
+  private document = inject(DOCUMENT);
+  private window = this.document.defaultView;
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, { username, password },this.httpOptions)
+      .pipe(
+        tap((response: AuthResponse) => {
+          this.window?.localStorage.setItem('token', response.token);
+          this.window?.localStorage.setItem('username', response.username);
+        })
+      );
+  }
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData, this.httpOptions)
+      .pipe(
+        tap(response => {
+          console.log('Registration response:', response);
+        })
+      );
+  }
+
+  logout(): void {
+    this.window?.localStorage.removeItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.window?.localStorage.getItem('token');
+  }
+}
+  
